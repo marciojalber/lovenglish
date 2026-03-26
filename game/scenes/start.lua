95b8f3eebc0a6458920c89182febf2488275adea
Marcio Jalber <marcio.ferreira@inss.gov.br>
2026-03-26 14:26:30 -0300

    Atualizacao

--- game/scenes/start.lua
+++ game/scenes/start.lua
@@ -15,6 +15,8 @@ function scene:load()
     card_h  = 100
     
     Card1   = Card:new({w = card_w, h = card_h})
+    Card2   = Card:new({w = 50, h = 20})
+
     Text1   = Text:new({content = "CHOOSE YOUR GAME", x = x_half - card_w / 2 + 10, y = y_half - card_h / 2 + 10})
     Text2   = Text:new({content = "Command line:", x = 10, y = win_h - 50})
     Text3   = Text:new({content = "", x = 10, y = win_h - 30, typeable = true})
